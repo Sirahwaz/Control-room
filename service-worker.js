ce69dfa1b486de463625d4fe0c71d6dac5105514
Sirahwaz <116122510+Sirahwaz@users.noreply.github.com>
2026-09-26 17:57:20 +0330

    Use only current Cockpit assets and purge legacy UI cache

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,9 @@
-const CACHE="midad-neural-command-v20-20260926r";
-const CORE=[
-  "./","./index.html","./app-max.js","./styles-max.css",
-  "./midad-maximum-enhancements.js","./midad-maximum-enhancements.css",
-  "./midad-command-v2.css","./midad-apex.css","./midad-apex.js","./midad-mesh.css","./midad-mesh.js",
-  "./manifest.webmanifest","./midad-pwa-icon.svg"
-];
+const CACHE="midad-cockpit-v1-20260926c1";
+const CORE=["./","./index.html","./midad-cockpit.css","./midad-cockpit.js","./manifest.webmanifest","./midad-pwa-icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin||e.request.method!=="GET")return;
-  e.respondWith(fetch(e.request).then(r=>{
-    if(r.ok){const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone)).catch(()=>{})}
-    return r;
-  }).catch(()=>caches.match(e.request)));
+ const u=new URL(e.request.url);
+ if(u.origin!==location.origin||e.request.method!=="GET")return;
+ e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{})}return r}).catch(()=>caches.match(e.request)));
 });
