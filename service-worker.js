dcba270fc64be23f9f9e52b95e66e614afac4e84
Sirahwaz <116122510+Sirahwaz@users.noreply.github.com>
2026-09-26 16:19:43 +0330

    chore: rotate v16 service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="midad-command-v16-20260926";
+const CACHE="midad-command-v16-20260926b";
 const CORE=["./","./index.html","./midad-control-v16.css","./midad-control-v16.js","./midad-control-v16-interactions.js","./manifest.webmanifest","./midad-pwa-icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
