d72e95c1f486270c979a56e439cafc66ebfec87b
Sirahwaz <116122510+Sirahwaz@users.noreply.github.com>
2026-09-26 19:10:50 +0330

    Bump cockpit service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="midad-cockpit-v1-20260926c7";
+const CACHE="midad-cockpit-v1-20260926c8";
 const CORE=["./","./index.html","./midad-cockpit.css","./midad-cockpit.js","./manifest.webmanifest","./midad-pwa-icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
